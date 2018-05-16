514b77521af35161f1692c24d2c6398452d441bf
Antonio Garcia <agarciaf7@gmail.com>
2018-05-16 21:16:26 +0200

    Carga inicial

---
+++ src/main/java/org/agf/model/Task.java
@@ -0,0 +1,155 @@
+package org.agf.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.util.Date;
+
+@Entity
+public class Task implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column
+	private boolean due;
+
+	@Column
+	private boolean persistent;
+
+	@Column
+	private Long priority;
+
+	@Column(nullable = false)
+	private Date creation;
+
+	@Column
+	private Date deadline;
+
+	@Column
+	private String observaciones;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Task)) {
+			return false;
+		}
+		Task other = (Task) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isDue() {
+		return due;
+	}
+
+	public void setDue(boolean due) {
+		this.due = due;
+	}
+
+	public boolean isPersistent() {
+		return persistent;
+	}
+
+	public void setPersistent(boolean persistent) {
+		this.persistent = persistent;
+	}
+
+	public Long getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Long priority) {
+		this.priority = priority;
+	}
+
+	public Date getCreation() {
+		return creation;
+	}
+
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
+
+	public Date getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (name != null && !name.trim().isEmpty())
+			result += "name: " + name;
+		result += ", due: " + due;
+		result += ", persistent: " + persistent;
+		if (priority != null)
+			result += ", priority: " + priority;
+		if (observaciones != null && !observaciones.trim().isEmpty())
+			result += ", observaciones: " + observaciones;
+		return result;
+	}
+}
